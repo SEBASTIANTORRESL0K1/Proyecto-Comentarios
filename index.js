9d0e28a2769e916788160aca86cfb8d3009e556b
SEBASTIANTORRESL0K1 <svelasco13@ucol.mx>
2025-03-03 14:37:43 -0600

    Agregando para que los comentarios se puedan ver en varias cuentas

--- index.js
+++ index.js
@@ -1,107 +1,125 @@
-const btnSinCuenta=document.getElementById("btnSinCuenta");
-const btnEnviar=document.getElementById("btnEnviar");
-const btn=document.getElementById("btn");
-const credenciales=document.getElementById("credenciales");
+const btnSinCuenta = document.getElementById("btnSinCuenta");
+const btnEnviar = document.getElementById("btnEnviar");
+const btn = document.getElementById("btn");
+const credenciales = document.getElementById("credenciales");
 credenciales.classList.toggle("visibilidad");
 let usuarioActivo;
-let usuarios=[];
-function buscarUsuarios(usuarios){
-    let nombre=document.getElementById("nombre").value;
-    let password=document.getElementById("password").value;
-    for(let i=0;i<usuarios.length;i++){
-        if(usuarios[i].nombre==nombre&&usuarios[i].password==password){
+let usuarios = [];
+let comentarios = [];
+
+function buscarUsuarios(usuarios) {
+    let nombre = document.getElementById("nombre").value;
+    let password = document.getElementById("password").value;
+    for (let i = 0; i < usuarios.length; i++) {
+        if (usuarios[i].nombre == nombre && usuarios[i].password == password) {
             return usuarios[i].nombre;
         }
     }
     return null;
 }
-btnSinCuenta.addEventListener("click",()=>{
-    const btnSinCuenta=document.getElementById("btnSinCuenta");
-    const btnEnviar=document.getElementById("btnEnviar");
-    const btnRegistrarse=document.getElementById("btnRegistrarse");
+
+btnSinCuenta.addEventListener("click", () => {
+    const btnRegistrarse = document.getElementById("btnRegistrarse");
     btnEnviar.classList.toggle("visibilidad");
     btnSinCuenta.classList.toggle("visibilidad");
     btnRegistrarse.classList.toggle("visibilidad");
-})
-btnRegistrarse.addEventListener("click",()=>{
-    let res=buscarUsuarios(usuarios);
-    if(res!=null){
-        let app=document.getElementById("app");
+});
+
+btnRegistrarse.addEventListener("click", () => {
+    let res = buscarUsuarios(usuarios);
+    if (res != null) {
+        let app = document.getElementById("app");
         app.classList.toggle("visibilidad");
-        const btnRegistrarse=document.getElementById("btnRegistrarse");
+        const btnRegistrarse = document.getElementById("btnRegistrarse");
         btnRegistrarse.classList.toggle("visibilidad");
-        let credenciales=document.getElementById("credenciales");
+        let credenciales = document.getElementById("credenciales");
         credenciales.classList.toggle("visibilidad");
-       
-    } else{
-        let nombre=document.getElementById("nombre").value;
-        let password=document.getElementById("password").value;
-        usuarios.push({nombre:nombre, password:password});
-        document.getElementById("password").value="";
-        document.getElementById("nombre").value="";
-        const btnSinCuenta=document.getElementById("btnSinCuenta");
-        const btnEnviar=document.getElementById("btnEnviar");
-        const btnRegistrarse=document.getElementById("btnRegistrarse");
+    } else {
+        let nombre = document.getElementById("nombre").value;
+        let password = document.getElementById("password").value;
+        usuarios.push({ nombre: nombre, password: password });
+        document.getElementById("password").value = "";
+        document.getElementById("nombre").value = "";
+        const btnSinCuenta = document.getElementById("btnSinCuenta");
+        const btnEnviar = document.getElementById("btnEnviar");
+        const btnRegistrarse = document.getElementById("btnRegistrarse");
         btnEnviar.classList.toggle("visibilidad");
         btnSinCuenta.classList.toggle("visibilidad");
         btnRegistrarse.classList.toggle("visibilidad");
     }
+});
 
-})
-btnEnviar.addEventListener("click",()=>{
-    let res=buscarUsuarios(usuarios);
-    if(res!=null){
-        let app=document.getElementById("app");
+btnEnviar.addEventListener("click", () => {
+    let res = buscarUsuarios(usuarios);
+    if (res != null) {
+        let app = document.getElementById("app");
         app.classList.toggle("visibilidad");
-        const btnRegistrarse=document.getElementById("btnRegistrarse");
+        const btnRegistrarse = document.getElementById("btnRegistrarse");
         btnRegistrarse.classList.toggle("visibilidad");
-        let credenciales=document.getElementById("credenciales");
+        let credenciales = document.getElementById("credenciales");
         credenciales.classList.toggle("visibilidad");
-        usuarioActivo=res;
-    } else{
-        setTimeout(()=>{
+        usuarioActivo = res;
+        if (comentarios.length > 0) {
+            let cont = document.getElementById("container");
+            for (let i = 0; i < comentarios.length; i++) {
+                console.log(comentarios[i]);
+                cont.innerHTML += `<div id="div${i}" class="comment"><p id='p${i}' class='parrafo'>${comentarios[i].comentario}</p><button class='btn' onclick="EliminarComentario(${i})">Eliminar</button><div class="time"><span>Fecha:${comentarios[i].fecha} </span><span>Hora: ${comentarios[i].hora}</span><span>👤:${comentarios[i].nombre}</span></div></div>`;
+            }
+        }
+    } else {
+        setTimeout(() => {
             alert("Usuario o contraseña incorrectos");
-        },1000);
+        }, 1000);
     }
 });
 
-
-let contador=0;
-btn.addEventListener("click",()=>{
-    let cont=document.getElementById("container");
-    let text=document.getElementById("text").value;
-    let fechaActual= new Date();
-    if(contador==0&&revisarSiEstaVacio()){
-        cont.innerHTML+=`<div id="div${contador}" class="comment"><p id='p${contador}' class='parrafo'>${text}</p><button class='btn' onclick="EliminarComentario(${contador})">Eliminar</button><div class="time"><span>Fecha: ${fechaActual.getDate()}/${fechaActual.getMonth()}/${fechaActual.getFullYear()}</span><span>Hora: ${fechaActual.getHours()}:${fechaActual.getSeconds()}</span><span>👤:${usuarioActivo}</span></div></div>`;
-        
+let contador = 0;
+btn.addEventListener("click", () => {
+    let cont = document.getElementById("container");
+    let text = document.getElementById("text").value;
+    let fechaActual = new Date();
+    if (contador == 0 && revisarSiEstaVacio()) {
+        cont.innerHTML += `<div id="div${contador}" class="comment"><p id='p${contador}' class='parrafo'>${text}</p><button class='btn' onclick="EliminarComentario(${contador})">Eliminar</button><div class="time"><span>Fecha: ${fechaActual.getDate()}/${fechaActual.getMonth() + 1}/${fechaActual.getFullYear()}</span><span>Hora: ${fechaActual.getHours()}:${fechaActual.getMinutes()}</span><span>👤:${usuarioActivo}</span></div></div>`;
+        comentarios.push({ nombre: usuarioActivo, comentario: text, fecha: `${fechaActual.getDate()}/${fechaActual.getMonth() + 1}/${fechaActual.getFullYear()}`, hora: `${fechaActual.getHours()}:${fechaActual.getMinutes()}` });
         contador++;
-    } else{
-        if(revisarSiEstaVacio()){
-            let nuevoDiv=document.createElement("div");
+        console.log(comentarios);
+    } else {
+        if (revisarSiEstaVacio()) {
+            let nuevoDiv = document.createElement("div");
             nuevoDiv.classList.add("comment");
-            nuevoDiv.id=`div${contador}`;
-            let nuevoParrafo=document.createElement("p");
-            nuevoDiv.innerHTML=`<p id='p${contador}' class='parrafo'>${text}</p><button class='btn' onclick="EliminarComentario(${contador})">Eliminar</button><div class="time"><span>Fecha: ${fechaActual.getDate()}/${fechaActual.getMonth()}/${fechaActual.getFullYear()}</span><span>Hora: ${fechaActual.getHours()}:${fechaActual.getSeconds()}</span><span>👤:${usuarioActivo}</span></div>`;
+            nuevoDiv.id = `div${contador}`;
+            nuevoDiv.innerHTML = `<p id='p${contador}' class='parrafo'>${text}</p><button class='btn' onclick="EliminarComentario(${contador})">Eliminar</button><div class="time"><span>Fecha: ${fechaActual.getDate()}/${fechaActual.getMonth() + 1}/${fechaActual.getFullYear()}</span><span>Hora: ${fechaActual.getHours()}:${fechaActual.getMinutes()}</span><span>👤:${usuarioActivo}</span></div>`;
             cont.insertBefore(nuevoDiv, cont.firstChild);
             contador++;
-           
-        } else{
+            comentarios.push({ nombre: usuarioActivo, comentario: text, fecha: `${fechaActual.getDate()}/${fechaActual.getMonth() + 1}/${fechaActual.getFullYear()}`, hora: `${fechaActual.getHours()}:${fechaActual.getMinutes()}` });
+            console.log(comentarios);
+        } else {
             alert("No puedes enviar un comentario vacío");
         }
-        
     }
-    document.getElementById("text").value="";});
-function revisarSiEstaVacio(){
-    let text=document.getElementById("text").value;
-    if(text==""){
-       return false;
-    } else{
-        return true;
-    }
+    document.getElementById("text").value = "";
+});
+
+function revisarSiEstaVacio() {
+    let text = document.getElementById("text").value;
+    return text !== "";
 }
-function EliminarComentario(id){
-    let cont=document.getElementById("container");
-    let div=document.getElementById(`div${id}`);
+
+function EliminarComentario(id) {
+    let cont = document.getElementById("container");
+    let div = document.getElementById(`div${id}`);
     cont.removeChild(div);
 }
-function recuperarNombreUsuario(){}
+
+const btnSalir = document.getElementById("btnSalir");
+btnSalir.addEventListener("click", () => {
+    let app = document.getElementById("app");
+    app.classList.toggle("visibilidad");
+    let credenciales = document.getElementById("credenciales");
+    credenciales.classList.toggle("visibilidad");
+    let btnRegistrarse = document.getElementById("btnRegistrarse");
+    btnRegistrarse.classList.toggle("visibilidad");
+    let cont = document.getElementById("container");
+    cont.innerHTML = "";
+    usuarioActivo=null;
+});
